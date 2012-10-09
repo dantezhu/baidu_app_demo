95abafe085b4f159bd39f9aba4eea8f132ade2c3
dantezhu <zny2008@gmail.com>
2012-10-10 01:48:01 +0800

    调通~

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from flask import request, session
 
 from baidu_api import BaiduAPI
 
+SECRET_KEY = '2334'
+
 BD_APPID = 385894
 BD_API_KEY = '45EIK7cZSSuQovKreyQHQnwz'
 BD_SECRET_KEY = 'Uy2uF4PItYIZcVtIllqAvcn1y2wZiVRO'
@@ -18,9 +20,12 @@ app.config.from_object(__name__)
 
 baidu_api = BaiduAPI(BD_APPID, BD_API_KEY, BD_SECRET_KEY)
 
-def auth_login():
+def get_login_uid():
     if 'bd_user' not in request.values or 'bd_sig' not in request.values:
-        return False
+        #如果没有参数，那有session也行
+        if 'uid' in session:
+            return session['uid']
+        return None
 
     bd_user = request.values['bd_user']
     bd_sig = request.values['bd_sig']
@@ -28,24 +33,30 @@ def auth_login():
     expected_sig = hashlib.md5('bd_user=%s%s' % (bd_user, BD_SECRET_KEY)).hexdigest()
 
     if expected_sig != bd_sig:
-        return False
+        return None
 
-    s_bd_user = session.get('bd_user', None)
+    uid = session.get('uid', None)
 
-    return s_bd_user == bd_user
+    if uid != bd_user:
+        #说明换了用户了，需要重新授权
+        return None
+
+    return uid
 
 @app.route('/index')
 def index():
-    is_login = auth_login()
+    login_uid = get_login_uid()
+
+    session.pop('uid', None)
 
     authorize_url = None
 
-    if not is_login:
+    if not login_uid:
         authorize_url = baidu_api.get_authorize_url(
             'http://%s%s' % (request.host, url_for('.login_callback'))
         )
 
-    return render_template('index.html', is_login=is_login, authorize_url=authorize_url)
+    return render_template('index.html', login_uid=login_uid, authorize_url=authorize_url)
 
 @app.route('/login_callback')
 def login_callback():
@@ -67,7 +78,9 @@ def login_callback():
     if not json_data:
         return u'获取用户资料报错'
 
-    return str(json_data)
+    session['uid'] = json_data['uid']
+
+    return render_template('login_callback.html')
 
 
 if __name__ == '__main__':
